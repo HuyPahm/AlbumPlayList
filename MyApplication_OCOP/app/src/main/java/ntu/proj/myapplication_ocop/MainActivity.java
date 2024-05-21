27199a4d9460701423e2fe176d581377e643a1d2
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-05-21 20:32:00 +0700

    build

--- MyApplication_OCOP/app/src/main/java/ntu/proj/myapplication_ocop/MainActivity.java
+++ MyApplication_OCOP/app/src/main/java/ntu/proj/myapplication_ocop/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.proj.myapplication_ocop;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText input;
+    private Button btn;
+    private databaseReference rootdatabaseref;
+    private DatabaseReference
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +29,14 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        input=findViewById(R.id.input);
+        btn=findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data=input.getText().toString();
+            }
+        });
     }
 }
